e31644dfd553398f3f86b4fd7940bd31cf995c84
dinhhn2000 <dinhhongoc@gmail.com>
2021-12-19 13:41:22 +0700

    Invalid command test case

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,19 +1,34 @@
+import com.github.stefanbirkner.systemlambda.Statement;
 import org.jetbrains.annotations.NotNull;
 
 public class Command {
-//    char action;
+  private static final char QUIT = 'Q';
+  private static final char CREATE_CANVAS = 'C';
+  private static final char LINE = 'L';
+  private static final char RECTANGLE = 'R';
+  private static final char FILL = 'B';
 
   public Command() {
     System.out.print("enter command: ");
   }
 
   public void input(@NotNull String command) {
-    System.out.println(command);
     switch (command.charAt(0)) {
-      case 'Q': {
-        System.exit(0);
-        break;
+      case CREATE_CANVAS -> {
       }
+
+//      case LINE: {
+//        break;
+//      }
+//      case RECTANGLE: {
+//        break;
+//      }
+//      case FILL: {
+//        break;
+//      }
+      case QUIT -> System.exit(0);
+      default -> System.err.println("Incorrect command");
     }
   }
+
 }
--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -1,20 +1,27 @@
 import org.junit.jupiter.api.Test;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.catchSystemExit;
-import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
 
 public class CommandTest {
   Command command = new Command();
 
   @Test
   void initCommand() throws Exception {
-    String text = tapSystemOut(Command::new);
-    assert text.equals("enter command: ");
+    String actualMessage = tapSystemOut(Command::new);
+    String expectedMessage = "enter command: ";
+    assert actualMessage.contains(expectedMessage);
   }
 
   @Test
   void enterQuitCommand() throws Exception {
     int statusCode = catchSystemExit(() -> command.input("Q"));
     assert statusCode == 0;
+  }
+
+  @Test
+  void enterInvalidCommand() throws Exception {
+    String actualMessage = tapSystemErr(() -> command.input("INVALID_COMMAND"));
+    String expectedMessage = "Incorrect command";
+    assert actualMessage.contains(expectedMessage);
   }
 }
