35ec5aa62c2e9f262b659ca2ddbf69cab4573397
dinhhn2000 <dinhhongoc@gmail.com>
2021-12-27 22:50:27 +0700

    More test cases about convertStringToInteger in Helpers

--- src/test/java/UtilsTest/HelperTest.java
+++ src/test/java/UtilsTest/HelperTest.java
@@ -5,9 +5,27 @@ import org.junit.jupiter.api.Test;
 
 public class HelperTest {
   @Test
+  public void convertStringToInteger() throws Exception {
+    String input = "123";
+    assert Helpers.convertStringToInteger(input) == 123;
+
+    input = "-987654321";
+    assert Helpers.convertStringToInteger(input) == -987654321;
+  }
+
+  @Test
   public void convertInvalidStringToInteger() {
-    String input = "INVALID";
+    String input = null;
+    // Case string contains letters
     try {
+      input = "INVALID";
+      Helpers.convertStringToInteger(input);
+    } catch (Exception e) {
+      assert e.getMessage().equals("Cannot convert String " + input + " to Integer");
+    }
+    // Case string is float
+    try {
+      input = "123.456";
       Helpers.convertStringToInteger(input);
     } catch (Exception e) {
       assert e.getMessage().equals("Cannot convert String " + input + " to Integer");
